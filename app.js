57180082955f5b8aa230b4def333c84d92374539
Saif <saif@Saifs-MacBook-Air.local>
2022-10-12 12:43:00 +0300

    handling errors and dotenv setup

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const request = require("postman-request");
 const chalk = require("chalk");
-const url =
-  "http://api.weatherstack.com/current?access_key=e482a6533ca1f3278064712ef2a7b7ff&query=37.8267,-122.4233";
+require("dotenv").config();
 
-request(url, { json: true }, (err, res) => {
+const urlWS = `http://api.weatherstack.com/current?access_key=${process.env.weather_stack}&query=37.8267,-122.4233`;
+
+request(urlWS, { json: true }, (err, res) => {
   if (err) {
-    console.log(err.errno);
+    console.log("Unable to connect to weather service!");
+  } else if (res.body.error) {
+    console.log(chalk.red.inverse("Unable to find location!"));
   } else {
     const data = res?.body;
-    if (data.success === false) {
-      console.log(chalk.red.inverse("error happened try again later"));
-    } else {
-      console.log(
-        `${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature}. It feels like ${data.current.feelslike}.`
-      );
-    }
+    console.log(
+      `${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature}. It feels like ${data.current.feelslike}.`
+    );
+  }
+});
+
+const urlMB = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.mapbox_key}&limit=1`;
+
+request(urlMB, { json: true }, (err, res) => {
+  if (err) {
+    console.log("Unable to connect to location service!");
+  } else if (res.body.message || !res.body.features[0]) {
+    console.log(chalk.red.inverse("Unable to find location!"));
+  } else {
+    const data = res.body;
+    console.log(`${data.features[0]?.text}`);
   }
 });
